6dd8ae0e5eb5523051f8fbbede37db3a0553f94e
Tania <nadsanp@gmail.com>
2024-11-19 11:24:20 +0100

    start the dice function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,19 @@ import Board from "./Board";
 import Warehouse from "./Warehouse";
 import Dice from "./Dice";
 import Reset from "./Reset";
+//import React, { useState } from "react";
 
 function App() {
+  // const [groguPosition, setGroguPosition] = useState(0);
+  // const [mercancias, setMercancias] = useState(['Huevo', 'Galleta', 'Rana']);
+  // const [gameMessage, setGameMessage] = useState('En curso');
+  // const rollDice = () => {
+  //   const diceResult = Math.floor(Math.random() * 4) + 1;
+  //   if (diceResult === 4)
+  //     { setGroguPosition((prevPosition)=>prevPosition+1);
+  //       setGameMessage('Grogu ha avanzado una casilla.');
+  // }
+
   return (
     <>
       <Header />
